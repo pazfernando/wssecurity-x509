7dcb3f5134eb289130d62974ce6ae17d34f83c5c
pazfernando <fernandopaz@softwareevolutivo.com.ec>
2014-10-29 11:37:21 -0500

    Initial development

---
+++ src/main/java/se_examples/wssecurity_x509/App.java
@@ -0,0 +1,13 @@
+package se_examples.wssecurity_x509;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
